63c513f475b72306dc8b263b3d614c5260859b75
4legoria <107012532+4legoria@users.noreply.github.com>
2022-10-12 16:54:53 -0300

    Update Ver versiones de SMB en servers.ps1

--- Ver versiones de SMB en servers.ps1
+++ Ver versiones de SMB en servers.ps1
@@ -1,10 +1,11 @@
 ﻿get-content -Path "$env:HOMEDRIVE$env:HOMEPATH\lista-servers.txt"
 
 $listaservers = @(get-content  -Path "$env:HOMEDRIVE$env:HOMEPATH\lista-servers.txt")
+#agregar en la raíz de la carpeta de usuario, un archivo txt con todos los equipos que queremos revisar que tengaa el nombre de lista-servers, también deberemos tener permisos administrativos para ejecutarlo.
 
 foreach ($server in $listaservers)
 {
 	write-host "server: $server"
 	Get-SmbConnection -ServerName $server | select Servername, ShareName, Dialect
 }  
- 
+#esto imprimirá el servidor que está revisando, luego nos dirá la información que seleccionemos
