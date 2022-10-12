92cf4d78ca6656e3cc063f335d5e320c75b9f7e0
4legoria <107012532+4legoria@users.noreply.github.com>
2022-10-12 16:52:56 -0300

    Inicial
    Script de Powershell para luego de parsear una lista de equipos, devuelva la versión de SMB que usan.

---
+++ Ver versiones de SMB en servers.ps1
@@ -0,0 +1,10 @@
+﻿get-content -Path "$env:HOMEDRIVE$env:HOMEPATH\lista-servers.txt"
+
+$listaservers = @(get-content  -Path "$env:HOMEDRIVE$env:HOMEPATH\lista-servers.txt")
+
+foreach ($server in $listaservers)
+{
+	write-host "server: $server"
+	Get-SmbConnection -ServerName $server | select Servername, ShareName, Dialect
+}  
+ 
